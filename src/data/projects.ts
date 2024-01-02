667a499b30e797da6f1c0f59917771c906d4d3f4
macbook kal <arnaud.pantenier@gmail.com>
2024-01-02 23:18:50 +0100

    ajout huilevak

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -11,7 +11,7 @@ export const projects: Card[] = [
     {
         img: "./Site1.png",
         title: "Racks à pinard",
-        text: "Application de gestion de bouteilles de vin (En développement)",
+        text: "Application de gestion de bouteilles de vin",
         code: "https://github.com/arnaudpant/ts-rack-pinard",
         tech: ["React", "TypeScript", "Firebase"],
         demo: "https://rack-a-pinard.web.app/",
@@ -25,20 +25,19 @@ export const projects: Card[] = [
         demo: "https://www.joelle-corda-naturopathe.fr/index.html",
     },
     {
+        img: "./Site5.png",
+        title: "Huile",
+        text: "Site vitrine d'un artisan",
+        code: "",
+        tech: ["React", "Figma"],
+        demo: "https://huilevak-demo.netlify.app/",
+    },
+    {
         img: "./Site3.png",
         title: "Clone Netflix",
         text: "Clone du site de Netflix",
         code: "https://github.com/arnaudpant/ts-netflix",
         tech: ["React", "TypeScript", "Firebase", "MUI", "React Query"],
         demo: "",
-    },
-    {
-        // TODO: Site responsive
-        img: "./Site2.png",
-        title: "Kal Agency",
-        text: "Site vitrine d'une agence web",
-        code: "https://github.com/arnaudpant/react-kal-agency",
-        tech: ["React", "redux"],
-        demo: "https://kal-agency.netlify.app/",
     },
 ];
